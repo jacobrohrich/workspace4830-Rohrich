96a189a03b7b785eabf84c16040cf946c9f0b383
jacobrohrich <89364826+jacobrohrich@users.noreply.github.com>
2023-10-13 19:57:16 -0500

    Commits from Eclipse

--- src/main/java/ProjectServlet.java
+++ src/main/java/ProjectServlet.java
@@ -1,41 +1,80 @@
-
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ProjectServlet
- */
-@WebServlet("/ProjectServlet")
+@WebServlet("/MyServletDB")
 public class ProjectServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProjectServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   private static final long serialVersionUID = 1L;
+   static String url = "jdbc:mysql://ec2-35-175-240-218.compute-1.amazonaws.com:3306/myDBRohrich";
+   static String user = "jacobremote";
+   static String password = "rohrich";
+   static Connection connection = null;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World");
-	}
+   public ProjectServlet() {
+      super();
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html;charset=UTF-8");
+      
+      response.getWriter().println("-------- MySQL JDBC Connection Testing ------------<br>");
+      response.getWriter().println(url);
+      response.getWriter().println(user);
+      response.getWriter().println(password);
+      try {
+         Class.forName("com.mysql.cj.jdbc.Driver"); //old:com.mysql.jdbc.Driver
+      } catch (ClassNotFoundException e) {
+         System.out.println("Where is your MySQL JDBC Driver?");
+         e.printStackTrace();
+         return;
+      }
+      response.getWriter().println("MySQL JDBC Driver Registered!<br>");
+      connection = null;
+      try {
+         connection = DriverManager.getConnection(url, user, password);
+      } catch (SQLException e) {
+         System.out.println("Connection Failed! Check output console");
+         e.printStackTrace();
+         return;
+      }
+      if (connection != null) {
+         response.getWriter().println("You made it, take control your database now!<br>");
+      } else {
+         System.out.println("Failed to make connection!");
+      }
+      try {
+         String selectSQL = "SELECT * FROM myTableRohrich";
+//         String theUserName = "user%";
+         response.getWriter().println(selectSQL + "<br>");
+         response.getWriter().println("------------------------------------------<br>");
+         PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+//         preparedStatement.setString(1, theUserName);
+         ResultSet rs = preparedStatement.executeQuery();
+         while (rs.next()) {
+            String id = rs.getString("ID");
+            String username = rs.getString("MYUSER");
+            String email = rs.getString("EMAIL");
+            String phone = rs.getString("PHONE");
+            response.getWriter().append("USER ID: " + id + ", ");
+            response.getWriter().append("USER NAME: " + username + ", ");
+            response.getWriter().append("USER EMAIL: " + email + ", ");
+            response.getWriter().append("USER PHONE: " + phone + "<br>");
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
 
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
 }
